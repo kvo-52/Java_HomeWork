86ab76157234eac4545b080533bfdace8e6ec359
kvo.52@mail.ru <kvo.52@mail.ru>
2023-02-13 22:53:11 +0300

    new metod Triangl

--- TriangleNumber.java
+++ TriangleNumber.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class TriangleNumber {
     public static void main(String[] args) {
-        
+        Scanner Scanner =new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int number=Scanner.nextInt();
+        Scanner.close();
+        System.out.println ("Треугольное число = "+Triangle(number)+ "\n" );
+    }
+    public static int Triangle(int num){
+        if (num==1){
+            return 1;
+        }
+        return Triangle(num-1)+num;
     }
 }
