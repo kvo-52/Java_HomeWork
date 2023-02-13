e32eca989cbb8a31c24b251981053d9e2f66c7a3
kvo.52@mail.ru <kvo.52@mail.ru>
2023-02-13 22:51:45 +0300

    new metod

---
+++ arrNumber.java
@@ -0,0 +1,9 @@
+// дан массив чисел, нужно сформировать новую выборку 
+// из только четных чисел
+public class arrNumber {
+    public static void main(String[] args) {
+
+       
+    
+}
+}
---
+++ program.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class program {
+public static void main(String[] args) {
+Scanner iScanner = new Scanner(System.in);
+System.out.printf("name: ");
+String name = iScanner.nextLine();
+System.out.printf("Привет, %s!", name);
+iScanner.close();
+}
+}
