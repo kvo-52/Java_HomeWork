f6535be3330b6a41de7cfef7486488cb5d3704d3
kvo.52@mail.ru <kvo.52@mail.ru>
2023-02-27 13:35:15 +0300

    mew metod createArray

---
+++ Exp03_MetodSortMerge.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+// 1. Метод создания массива
+// 2. Метод сортировки слиянием
+// 3. Печать результата
+
+public class Exp03_MetodSortMerge {
+    public static void main(String[] args) throws IOException{
+       
+    }
+
+public static ArrayList<Integer> createArray() {
+    Random randomArray= new Random();
+    int size=randomArray.nextInt(10,30);
+    ArrayList <Integer> myArray= new ArrayList<> (size);
+    for (int i=0; i<size; i++){array.add(i, (randomArray.nextInt(100)))}
+    return myArray;
+}
+    }
+
+
+    
+    
